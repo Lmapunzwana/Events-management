65480a885b85c6a9f90b45c92c0760fc4089df58
ecknyce <phiriecknycem@gmail.com>
2025-04-12 14:44:08 +0200

    Added models and migrations for events and registrations

---
+++ database/migrations/2025_04_12_113438_create_events_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->time('Time')->nullable();
+            $table->date('Event_Date')->nullable();
+            $table->string('Location');
+            $table->string('Event_Description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
