b59c5298ef5791cd3fe9331e056422117221858d
Jemade <mapasurejayden@gmail.com>
2025-04-12 22:21:49 +0200

    commit message

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -8,6 +8,8 @@ use Inertia\Inertia;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
+use Carbon\Carbon;
+
 {
     public function index(){
         $events = Event::all();
@@ -18,6 +20,14 @@ class EventController extends Controller
             'events' => $events,
             'registeredEvents' => $registeredEvents
         ]);
+
+        // now , showing the upcoming events in our project
+        //we will be using Carbon (already included with Laravel) to get the current date and only shows events that are today or in the future, sorted by date.
+
+            $today = Carbon::today();
+            $events = Event::whereDate('date', '>=', $today)->orderBy('date')->get();
+            return view('events.index', compact('events'));
+        
     }
 
     public function api() {
