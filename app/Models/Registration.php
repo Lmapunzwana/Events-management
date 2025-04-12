f2da71e62cf7ee51fcc2ef72a1b144fdfdaaa70b
ecknyce <phiriecknycem@gmail.com>
2025-04-12 15:28:26 +0200

    Added models and migrations for events and registrations

--- app/Models/Registration.php
+++ app/Models/Registration.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Registration extends Model
 {
-    //
+   public function User(){
+        return $this->belongsTo(User::class);
+   } 
+
+    public function Event(){
+          return $this->belongsTo(Event::class);
+    }
 }
--- app/Models/event.php
+++ app/Models/event.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class event extends Model
 {
-    //
+    public function Registration()
+    {
+        return $this->hasMany(Registration::class);
+    }
+   
+    
 }
